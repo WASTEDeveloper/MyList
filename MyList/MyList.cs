4fc9056b3515c7d31559a387366eeddbc1284483
WASTEDeveloper <a.shabalinski@gmail.com>
2021-02-01 13:32:45 +0300

    Добавьте файлы проекта.

---
+++ MyList/MyList.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyList
+{
+    public class MyList
+    {
+        public List<int> myList;
+
+        public MyList()
+        {
+            myList = new List<int>();
+        }
+
+        public void MyAdd(int num)
+        {
+            myList.Add(num);
+        }
+
+        public void MyOutput()
+        {
+            foreach (var item in myList)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+
+        public int MyMax()
+        {
+            return myList.Max();
+        }
+
+        public int[] GetElements(Func<int, bool> areEven)
+        {
+            int[] evenElements = new int[myList.FindAll((x) => x % 2 == 0).Count];
+            int i = 0;
+            foreach (var item in myList)
+            {
+                if (areEven(item))
+                {
+                    evenElements[i] = item;
+                    i++;
+                }
+            }
+            return evenElements;
+        }
+    }
+}
---
+++ MyListTest/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyList;
+
+namespace MyListTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList.MyList list = new MyList.MyList();
+
+            Console.WriteLine("Ввдите кол-во добовляемых значений: ");
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                list.MyAdd(int.Parse(Console.ReadLine()));
+            }
+
+            list.MyOutput();
+
+            Console.WriteLine($"Максимальное значение: {list.MyMax()}");
+
+            int[] mas = list.GetElements((x) => x % 2 == 0);
+            for (int i = 0; i < mas.Length; i++)
+            {
+                Console.Write(mas[i] + " ");
+            }
+            Console.WriteLine();
+
+            Console.ReadLine();
+        }
+    }
+}
